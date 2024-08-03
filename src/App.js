a1bad3fc4b4ff12c90e1b5ce4fcca666617eda1a
abyanardiatama <abyanardiatama@students.undip.ac.id>
2024-08-03 13:13:27 +0700

    create and nesting element

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function MyButtton() {
+    return (
+        <button>
+            Click Me!
+        </button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButtton />
+        </div>
+    );
+}
