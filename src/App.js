04ecc2253f39b4e2d31be61a691287749c0d4420
abyanardiatama <abyanardiatama@students.undip.ac.id>
2024-08-03 14:04:02 +0700

    counters that update together

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
-// 4. BUTTON COUNTER UPDATE TOGETHER
-function MyButton() {
-    const [count, setCount] = useState(0);
-    function handleClick() {
-        setCount(1 + count);
-    }
+// 5. BUTTON COUNTER UPDATE SEPARATELY
+function MyButton({count, onClick}) {
     return (
-        <button onClick={handleClick}>
-            Click {count} times
+        <button onClick={onClick}>
+            Clicked {count} times
         </button>
     );
 }
 
 export default function MyApp() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(1 + count);
+    }
     return (
         <div>
-            <h1>Counters that update separately</h1>
-            <MyButton />
-            <MyButton />
+            <h1>Counters that update together</h1>
+            <MyButton count={count} onClick={handleClick} />
+            <MyButton count={count} onClick={handleClick} />
         </div>
     );
 }
