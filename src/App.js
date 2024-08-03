1b62ad70ac133a52cdafd65c988bddd121b6feb0
abyanardiatama <abyanardiatama@students.undip.ac.id>
2024-08-03 14:03:11 +0700

    counters that update separately

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-// 3. CONDITIONAL RENDERING
-const products = [
-    { title: 'Cabbage', isFruit: false, id: 1},
-    { title: 'Garlic', isFruit: false, id:2 },
-    { title: 'Apple', isFruit: true, id:3 },
-];
-
-export default function ShoppingList() {
-    const listItems = products.map(product =>
-        <li
-            key={product.id}
-            style={{ 
-                color: product.isFruit ? 'magenta' : 'darkgreen'
-             }}
-        >
-            {product.title}
-        </li>
-    );
-
+import { useState } from "react";
+// 4. BUTTON COUNTER UPDATE TOGETHER
+function MyButton() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(1 + count);
+    }
     return (
-        <ul>
-            {listItems}
-        </ul>
+        <button onClick={handleClick}>
+            Click {count} times
+        </button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Counters that update separately</h1>
+            <MyButton />
+            <MyButton />
+        </div>
     );
 }
