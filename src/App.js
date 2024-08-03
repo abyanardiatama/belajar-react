354fd0b64a046a5d8138dc6a6f688db248b3eb9d
abyanardiatama <abyanardiatama@students.undip.ac.id>
2024-08-03 13:58:23 +0700

    write markup with jsx

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-// 1. CREATE AND NESTING ELEMENT
-function MyButtton() {
-    return (
-        <button>
-            Click Me!
-        </button>
-    );
-}
+// 2. WRITE MARKUP WITH JSX
+const user = {
+    name: 'Hedy Lamar',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
 
 export default function MyApp() {
     return (
-        <div>
-            <h1>Welcome to my app</h1>
-            <MyButtton />
-        </div>
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of' + user.name}
+                style={{ 
+                    width: user.imageSize,
+                    height: user.imageSize
+                 }}
+            />
+        </>
     );
 }
